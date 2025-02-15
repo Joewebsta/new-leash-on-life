1b76db634d9591f447a411c38adb63cf75492757
Joe Webster <joseph.scott.webster@gmail.com>
2025-02-15 15:03:54 -0500

    Create BASE_URL constant

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,18 +1,17 @@
 import { loginFormSchema } from "@/schemas/schema";
 import { z } from "zod";
 
+export const BASE_URL = "https://frontend-take-home-service.fetch.com";
+
 export async function performLogin(values: z.infer<typeof loginFormSchema>) {
-  const response = await fetch(
-    "https://frontend-take-home-service.fetch.com/auth/logi",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-      body: JSON.stringify(values),
-    }
-  );
+  const response = await fetch(`${BASE_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(values),
+  });
 
   if (!response.ok) {
     throw new Error(`Login failed: ${response.status} ${response.statusText}`);
