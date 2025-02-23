27a7c6747cb78e551185c568c9a94a1f47a626b0
Joe Webster <joseph.scott.webster@gmail.com>
2025-02-23 18:20:12 -0500

    Make logo clickable and link to dog search page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { usePerformLogout } from "@/services/authService";
 import { PawPrint, X } from "lucide-react";
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 import PulseLoader from "react-spinners/PulseLoader";
 
 export default function Layout() {
@@ -16,10 +16,12 @@ export default function Layout() {
     <div className="relative min-h-screen">
       <div className="px-6 md:px-10 xl:px-20 border-b border-neutral-200">
         <header className="flex h-12 items-center justify-between">
-          <div className="flex gap-2 items-center">
-            <PawPrint size={20} />
-            <h1 className="font-bold">A New Leash on Life</h1>
-          </div>
+          <Link to={"/dogs/search?sort=breed:asc"}>
+            <div className="flex gap-2 items-center">
+              <PawPrint size={20} />
+              <h1 className="font-bold">A New Leash on Life</h1>
+            </div>
+          </Link>
           <Button variant="ghost" onClick={async () => await logoutAsync()}>
             {isLogoutPending ? (
               <PulseLoader
