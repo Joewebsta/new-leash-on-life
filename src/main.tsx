c54db204157badb2c04291ca340ef4f7e971f898
Joe Webster <joseph.scott.webster@gmail.com>
2025-02-23 14:29:57 -0500

    Reorder and organize imports in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router";
+import App from "./App.tsx";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
