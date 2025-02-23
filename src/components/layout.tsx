741a518c3ba0ade9dacb0c9839bb2c3a09c9a788
Joe Webster <joseph.scott.webster@gmail.com>
2025-02-23 14:30:12 -0500

    Align header items vertically

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -12,7 +12,7 @@ export default function Layout() {
     <div className="relative min-h-screen">
       <div className="px-6 md:px-10 xl:px-20 border-b border-neutral-200">
         <header className="flex h-12 items-center justify-between">
-          <div className="flex gap-2">
+          <div className="flex gap-2 items-center">
             <PawPrint size={20} />
             <h1 className="font-bold">A New Leash on Life</h1>
           </div>
