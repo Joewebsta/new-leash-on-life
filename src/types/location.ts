83eaf767be346133d884d9ea42f4c79fe808ceb4
Joe Webster <joseph.scott.webster@gmail.com>
2025-07-11 14:00:39 -0400

    Create LocationSearchParams and LocationSearchResult types

--- src/types/location.ts
+++ src/types/location.ts
@@ -1,4 +1,4 @@
-export interface Location {
+interface Location {
   zip_code: string;
   latitude: number;
   longitude: number;
@@ -7,7 +7,36 @@ export interface Location {
   county: string;
 }
 
-export interface Coordinates {
+interface Coordinates {
   lat: number;
   lon: number;
 }
+
+interface LocationSearchParams {
+  city?: string;
+  states?: string[];
+  geoBoundingBox?: {
+    top?: number;
+    left?: number;
+    bottom?: number;
+    right?: number;
+    bottom_left?: Coordinates;
+    top_right?: Coordinates;
+    bottom_right?: Coordinates;
+    top_left?: Coordinates;
+  };
+  size?: number;
+  from?: number;
+}
+
+interface LocationSearchResult {
+  results: Location[];
+  total: number;
+}
+
+export type {
+  Coordinates,
+  Location,
+  LocationSearchParams,
+  LocationSearchResult,
+};
