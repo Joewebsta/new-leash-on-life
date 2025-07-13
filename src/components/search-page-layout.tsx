99733d2528879b2c6a18f02d641179a6dffb2104
Joe Webster <joseph.scott.webster@gmail.com>
2025-07-13 17:43:50 -0400

    Create SearchPageLayout component

---
+++ src/components/search-page-layout.tsx
@@ -0,0 +1,39 @@
+import { MobileMatchButton } from "@/components/mobile-match-button";
+import { SearchHeader } from "@/components/search-header";
+import { Dog } from "@/types/types";
+import { ReactNode } from "react";
+
+interface SearchPageLayoutProps {
+  children: ReactNode;
+  breeds: string[];
+  selectedDogs: Set<Dog>;
+  onNavigateToMatch: () => void;
+  onTabChange: (value: "browse-all" | "favorites") => void;
+  activeTab: "browse-all" | "favorites";
+}
+
+export const SearchPageLayout = ({
+  children,
+  breeds,
+  selectedDogs,
+  onNavigateToMatch,
+  onTabChange,
+  activeTab,
+}: SearchPageLayoutProps) => (
+  <div className="pb-[84px] sm:pb-0 px-6 md:px-10 xl:px-20 flex flex-col flex-1">
+    <div className="flex flex-col flex-1">
+      <SearchHeader
+        breeds={breeds}
+        selectedDogs={selectedDogs}
+        onNavigateToMatch={onNavigateToMatch}
+        onTabChange={onTabChange}
+        activeTab={activeTab}
+      />
+      {children}
+    </div>
+    <MobileMatchButton
+      selectedDogs={selectedDogs}
+      onNavigateToMatch={onNavigateToMatch}
+    />
+  </div>
+);
