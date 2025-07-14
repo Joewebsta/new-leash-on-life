54ef9108d161b8bb665fbaaf26ca16e8dd990d83
Joe Webster <joseph.scott.webster@gmail.com>
2025-07-14 15:55:35 -0400

    Optimize buildSearchParams with useMemo

--- src/components/search/filters-form.tsx
+++ src/components/search/filters-form.tsx
@@ -88,7 +88,10 @@ export function FiltersForm({
     return params;
   };
 
-  const searchParamsString = buildSearchParams(formValues).toString();
+  const searchParamsString = React.useMemo(
+    () => buildSearchParams(formValues).toString(),
+    [formValues]
+  );
 
   const {
     isPending: isLoadingSearchData,
