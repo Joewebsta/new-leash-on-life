4498f7cf86dba8214cfdf5e8b758bff874a1c885
Joe Webster <joseph.scott.webster@gmail.com>
2025-07-15 17:36:28 -0400

    Add BASE_URL constant to constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,3 +5,5 @@ export const FILTERS_FORM_CONSTANTS = {
   DEFAULT_SORT: "breed:asc" as const,
   SEARCH_DEBOUNCE_MS: 300,
 } as const;
+
+export const BASE_URL = "https://frontend-take-home-service.fetch.com";
