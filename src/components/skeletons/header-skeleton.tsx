2895c6cf39b13affc80f937ee75a8ef4a2313ef3
Joe Webster <joseph.scott.webster@gmail.com>
2025-07-15 18:22:57 -0400

    Reduce height of header skeleton elements

--- src/components/skeletons/header-skeleton.tsx
+++ src/components/skeletons/header-skeleton.tsx
@@ -4,10 +4,10 @@ export function HeaderSkeleton() {
   return (
     <div className="py-6 gap-3 flex justify-between">
       {/* Tabs skeleton */}
-      <Skeleton className="h-10 w-[185px] rounded-md" />
+      <Skeleton className="h-9 w-[185px] rounded-md" />
 
       {/* Filter button skeleton */}
-      <Skeleton className="h-10 w-[99px] rounded-md hidden sm:inline-flex" />
+      <Skeleton className="h-9 w-[99px] rounded-md" />
     </div>
   );
 }
