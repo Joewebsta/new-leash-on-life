d73d0da7539a6bc7f3b893fad4c291566cedcfc2
Joe Webster <joseph.scott.webster@gmail.com>
2025-07-15 09:32:49 -0400

    Create mutation for searching locations

--- src/services/locationService.ts
+++ src/services/locationService.ts
@@ -1,6 +1,6 @@
 import { fetchLocations, searchLocations } from "@/api/locations";
 import { LocationSearchParams } from "@/types/location";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 export function useFetchLocations(zipCodes: string[]) {
   return useQuery({
@@ -11,11 +11,17 @@ export function useFetchLocations(zipCodes: string[]) {
   });
 }
 
-export function useSearchLocations(params: LocationSearchParams) {
+export function useSearchLocationsMutation() {
+  return useMutation({
+    mutationFn: (params: LocationSearchParams) => searchLocations(params),
+  });
+}
+
+export function useSearchLocationsQuery(params: LocationSearchParams) {
   return useQuery({
     queryKey: ["locations", "search", params],
     queryFn: () => searchLocations(params),
-    enabled: !!(params.city || params.states || params.geoBoundingBox),
+    // enabled: !!(params.city || params.states || params.geoBoundingBox),
     staleTime: 5 * 60 * 1000,
   });
 }
