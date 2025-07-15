50c03381ec06d6dc0f5a81b77d791dfc976f8027
Joe Webster <joseph.scott.webster@gmail.com>
2025-07-15 09:31:02 -0400

    Remove mobile-match-button-skeleton

--- src/components/skeletons/mobile-match-button-skeleton.tsx
+++
@@ -1,9 +0,0 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
-export function MobileMatchButtonSkeleton() {
-  return (
-    <div className="fixed sm:hidden bottom-0 inset-x-0 p-6 bg-white border-t border-neutral-200 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)]">
-      <Skeleton className="w-full h-9 rounded-md" />
-    </div>
-  );
-}
--- src/components/states/loading-skeleton.tsx
+++ src/components/states/loading-skeleton.tsx
@@ -1,13 +1,11 @@
 import { DogGridSkeleton } from "@/components/skeletons/dog-grid-skeleton";
 import { HeaderSkeleton } from "@/components/skeletons/header-skeleton";
-import { MobileMatchButtonSkeleton } from "@/components/skeletons/mobile-match-button-skeleton";
 
 export function LoadingSkeleton() {
   return (
     <div className="pb-[130px] px-6 md:px-10 xl:px-20">
       <HeaderSkeleton />
       <DogGridSkeleton />
-      <MobileMatchButtonSkeleton />
     </div>
   );
 }
