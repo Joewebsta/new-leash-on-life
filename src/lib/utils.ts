ef11b1dc638d906f6ca6d69275c4861c754dc6e7
Joe Webster <joseph.scott.webster@gmail.com>
2025-07-16 08:38:53 -0400

    Update buildSearchParams and getDefaultFormValues to use FiltersFormData schema

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,19 +1,20 @@
-import { FormSchema } from "@/components/search/filters-form";
+import { FiltersFormData, Option } from "@/schemas/filters";
 import { FILTERS_FORM_CONSTANTS } from "@/constants/constants";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const buildSearchParams = (data: z.infer<typeof FormSchema>) => {
+export const buildSearchParams = (data: FiltersFormData) => {
   const params = new URLSearchParams();
 
   if (data.sort) params.append("sort", data.sort);
-  data.breeds.forEach((breed) => params.append("breeds", breed.value));
-  data.zipCodes.forEach((zipCode) => params.append("zipCodes", zipCode.value));
+  data.breeds.forEach((breed: Option) => params.append("breeds", breed.value));
+  data.zipCodes.forEach((zipCode: Option) =>
+    params.append("zipCodes", zipCode.value)
+  );
   params.append("ageMin", data.ageRange[0].toString());
   params.append("ageMax", data.ageRange[1].toString());
   return params;
@@ -23,7 +24,7 @@ export const getDefaultFormValues = (searchParams: URLSearchParams) => {
   const { DEFAULT_AGE_MIN, DEFAULT_AGE_MAX } = FILTERS_FORM_CONSTANTS;
 
   return {
-    sort: searchParams.get("sort") as z.infer<typeof FormSchema>["sort"],
+    sort: searchParams.get("sort") as FiltersFormData["sort"],
     breeds: searchParams
       .getAll("breeds")
       .map((breed) => ({ value: breed, label: breed })),
