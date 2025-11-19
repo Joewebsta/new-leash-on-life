4f5138aa04c401dedfe825f23b4fc6e2a385c82b
Joe Webster <joseph.scott.webster@gmail.com>
2025-11-19 11:42:12 -0500

    Toggle React Query Devtools initial state to closed

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={true} />
+        <ReactQueryDevtools initialIsOpen={false} />
         <App />
       </QueryClientProvider>
     </BrowserRouter>
